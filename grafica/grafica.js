a05c627ee0ec295e167a40051452343a55ad6cdf
Edwin leandro <Snorfil@hotmail.com>
2022-04-21 21:12:50 +0200

    Acabo de añadir un selector programaticamente con los nombres de binance

--- grafica/grafica.js
+++ grafica/grafica.js
@@ -1,4 +1,5 @@
  var lista = [];
+ var nombrePares = [];
 
  
  async function request(){
@@ -18,6 +19,37 @@
         }
     pintar() 
     //pintarPrueba()
+}
+async function pares(){
+    var req = new XMLHttpRequest();
+    var par = document.getElementsByTagName('h2')[0].innerText;
+    
+    await req.open('GET', 'https://api.binance.com/api/v3/ticker/price', false);
+    req.send(null);
+    if (req.status == 200)
+        dump(req.responseText);
+        let aux = JSON.parse(req.responseText);
+    
+        for (let i = 0; i < aux.length; i++) {
+            nombrePares.push(aux[i]['symbol'])  
+        }
+
+        var myParent = document.body;
+
+    //Create array of options to be added
+
+    //Create and append select list
+    var selectList = document.getElementById("selector");
+   
+
+    //Create and append the options
+    for (var i = 0; i < nombrePares.length; i++) {
+        var option = document.createElement("option");
+        option.value = nombrePares[i];
+        option.text = nombrePares[i];
+        selectList.appendChild(option);
+    }
+
 }
 function crearDataset(){
     return 
